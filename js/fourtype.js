90d24553b86f7e6d53c8320998341df8b8cddde9
pratikchasiya <pratikchasiya1297@gmail.com>
2023-10-15 17:45:49 +0530

    new

---
+++ js/fourtype.js
@@ -0,0 +1,5 @@
+/* getElementById avu lakhyu jem k first small hoy p6i capital hoy to ane camal word kehvay */
+document.getElementById("main").innerHTML = "Hello Pratik";  /* 1st type ==perticular id ma java mate */
+alert("hello");  /* 2nd type == hello nu pop up avse */
+document.write("hello pratik chasiya"); /* 3rd type ==aa type only normal script ma use thse ama async and defer ma use thse ni */
+console.log("PRATIK CHASIYA"); /* 4th type == aanu output jova mate inspect ma jai ne console ma jai ne jovu */
